445d88487bb460b4a4f8373f46605fc7aae3db81
YaelNorman1 <YaelNorman2808@gmail.com>
2022-12-01 14:24:18 +0200

    remove comments

--- BI_project/Backend/Routes/statistic_route.py
+++ BI_project/Backend/Routes/statistic_route.py
@@ -9,9 +9,7 @@ db_menager= MySqlManager()
 
 @route.get("/statistics/{user_id}/{category}", status_code=status.HTTP_200_OK)
 def get_user_statistics(user_id, category) -> dict :
-    num=5
     try:
-        # return "hello"
         return db_menager.get_user_statistics(user_id, category)
     except mysql.MySQLError as e:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=e)
