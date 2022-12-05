285a09e21f4217ff31ed0ed34ca97eb68e193703
yagelpinhas <yagel250@gmail.com>
2022-12-05 13:19:03 +0200

    changed to one scheduler - now amount anomalies work

---
+++ BI_project/Scheduler/bi_tables_manager.py
@@ -0,0 +1,74 @@
+from datetime import datetime,timedelta
+import pymysql
+import requests
+from constants import *
+
+connection = pymysql.connect(
+        host="localhost",
+        user="root",
+        password="",
+        db="anomaly_db",
+        charset="utf8",
+        cursorclass=pymysql.cursors.DictCursor
+)
+
+
+def get_user_statistics(user_id):
+        try:
+                with connection.cursor() as cursor:
+                        get_user_statistics = f"""
+                        SELECT *
+                        FROM user_statistics
+                        WHERE userId = {user_id} 
+                        """
+                        cursor.execute(get_user_statistics)
+                        connection.commit()
+                        result = cursor.fetchall()
+                        print(result)
+                        return result
+        except TypeError as e:
+                print(e)
+
+
+def add_anomaly(user_id,category,quantity,start_date,end_date):
+        try:
+                with connection.cursor() as cursor:
+                        add_anomaly_query = f"""
+                        INSERT INTO anomalies
+                        (anomalyId,userId,category,quantity,startDate,endDate) 
+                        VALUES (null,{user_id},'{category}',{quantity},%s,%s)
+                        """
+                        cursor.execute(add_anomaly_query, (start_date,end_date))
+                        connection.commit()
+        except TypeError as e:
+                print(e)
+
+def update_user_statistics(user_id, avg_num_withdraws, avg_num_deposits, avg_amount_withdraws, avg_amount_deposit):
+        try:
+                with connection.cursor() as cursor:
+                        update_user_statistic_query = f"""
+                        UPDATE user_statistics
+                        SET userId={user_id}, 
+                        avgNumOfWithdraws={avg_num_withdraws},
+                        avgNumOfDeposits={avg_num_deposits},
+                        avgAmountWithdraw={avg_amount_withdraws},
+                        avgAmountDeposit={avg_amount_deposit},
+                        numOfUpdates=numOfUpdates+1
+                        WHERE userId={user_id}
+                        """
+                        cursor.execute(update_user_statistic_query)
+                        connection.commit()
+        except TypeError as e:
+                print(e)
+
+def insert_user_statistics(user_id, avg_num_withdraws, avg_num_deposits, avg_amount_withdraws, avg_amount_deposit):
+        try:
+                with connection.cursor() as cursor:
+                        add_user_statistic_query = f"""
+                        INSERT INTO user_statistics
+                        VALUES ({user_id},{avg_num_withdraws},{avg_num_deposits},{avg_amount_withdraws},{avg_amount_deposit},1)
+                        """
+                        cursor.execute(add_user_statistic_query)
+                        connection.commit()
+        except TypeError as e:
+                print(e)
--- BI_project/Scheduler/scheduler.py
+++ BI_project/Scheduler/scheduler.py
@@ -1,63 +1,54 @@
 import schedule
 import time
 from constants import *
-import bi_api_manager
+import bi_tables_manager
 import transactions_table_manager
 from datetime import datetime,timedelta
 import requests
 
-async def check_amount_anomaly(user_id,quantity, category):
-    stats = await bi_api_manager.get_user_statistics(user_id)
+def check_amount_anomaly(user_id,quantity, category):
+    stats = bi_tables_manager.get_user_statistics(user_id)
     if not stats:
         return False
-    expected_value = stats[category]
-    return quantity> ((1 + AMOUNT_DEVIATION_PERCENTAGE) * expected_value)
+    expected_value = stats[0][category]
+    is_it_anomaly = quantity> ((1 + AMOUNT_DEVIATION_PERCENTAGE) * expected_value)
+    return is_it_anomaly
         
 
 def check_transactions_num_anomaly():
     pass
 
-async def update_anomalies_table():
-    now = datetime.now()
-    prev = now-timedelta(hours=0,minutes=0,seconds=ANOMALIES_SCHEDULER_TIME_PERIOD)
-    recent_transactions = transactions_table_manager.get_recent_transactions(ANOMALIES_SCHEDULER_TIME_PERIOD)
-    for transaction in recent_transactions: 
-        quantity = transaction["TransactionAmount"]
-        user_id = transaction["TransactionUserID"]
-        category = BI_EVENT_AVG_AMOUNT_DEPOSITS if quantity>0 else BI_EVENT_AVG_AMOUNT_WITHDRAWS
-        if await check_amount_anomaly(user_id,quantity, category):
-            bi_api_manager.add_anomaly(user_id,category,quantity,prev,now)
-
 def calc_amount_avg(prev, amount, num_of_updates):
     return (prev+amount)/(num_of_updates+1)
 
-async def update_user_statistics_table():
+def update_bi_database():
     recent_transactions = transactions_table_manager.get_recent_transactions(USER_STATISTICS_SCHEDULER_TIME_PERIOD)
     for transaction in recent_transactions:
         user_id = transaction["TransactionUserID"]
         quantity = transaction["TransactionAmount"]
         category = BI_EVENT_AVG_AMOUNT_DEPOSITS if quantity>0 else BI_EVENT_AVG_AMOUNT_WITHDRAWS
-        if (not await check_amount_anomaly(user_id,quantity, category)):
+        if (not check_amount_anomaly(user_id,quantity, category)):
              ##We need to add and not check transactions num anomaly
-            stats = bi_api_manager.get_user_statistics(user_id)
+            stats = bi_tables_manager.get_user_statistics(user_id)
             avg_num_withdraws = 0
             avg_num_deposits = 0
             if not stats:
                 avg_amount_withdraws = quantity if quantity<0 else 0
                 avg_amount_deposits = quantity if quantity>0 else 0
-                bi_api_manager.insert_user_statistics(user_id,avg_num_withdraws,avg_num_deposits,avg_amount_withdraws,avg_amount_deposits)
+                bi_tables_manager.insert_user_statistics(user_id,avg_num_withdraws,avg_num_deposits,avg_amount_withdraws,avg_amount_deposits)
             else:
-                num_of_updates = stats["numOfUpdates"]
-                prev_avg_amount_withdraw = stats["avgAmountWithdraw"]
-                prev_avg_amount_deposite = stats["avgAmountDeposit"]
+                num_of_updates = stats[0]["numOfUpdates"]
+                prev_avg_amount_withdraw = stats[0]["avgAmountWithdraw"]
+                prev_avg_amount_deposite = stats[0]["avgAmountDeposit"]
                 avg_amount_withdraws = calc_amount_avg(prev_avg_amount_withdraw, quantity, num_of_updates) if quantity<0 else avg_amount_withdraws
                 avg_amount_deposits = calc_amount_avg(prev_avg_amount_deposite, quantity, num_of_updates) if quantity>0 else avg_amount_deposits
-                bi_api_manager.update_user_statistics(user_id,avg_num_withdraws,avg_num_deposits,avg_amount_withdraws,avg_amount_deposits)
+                bi_tables_manager.update_user_statistics(user_id,avg_num_withdraws,avg_num_deposits,avg_amount_withdraws,avg_amount_deposits)
+        else:
+            now = datetime.now()
+            prev = now-timedelta(hours=0,minutes=0,seconds=ANOMALIES_SCHEDULER_TIME_PERIOD)
+            bi_tables_manager.add_anomaly(user_id,category,quantity,prev,now)
 
-
-
-# schedule.every(ANOMALIES_SCHEDULER_TIME_PERIOD).seconds.do(update_anomalies_table)
-schedule.every(USER_STATISTICS_SCHEDULER_TIME_PERIOD).seconds.do(update_user_statistics_table)
+schedule.every(USER_STATISTICS_SCHEDULER_TIME_PERIOD).seconds.do(update_bi_database)
 while True:
     schedule.run_pending()
     time.sleep(1)
